881ffcf6003e99f0364255c97ecc118c145b7564
Hai Ning <haining@Hais-MacBook-Pro.local>
2017-06-30 00:38:02 -0400

    initial check in

---
+++ sweep_sklearn.py
@@ -0,0 +1,26 @@
+from sklearn import grid_search, datasets
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.grid_search import GridSearchCV
+
+class timeit():
+    from datetime import datetime
+    def __enter__(self):
+        self.tic = self.datetime.now()
+    def __exit__(self, *args, **kwargs):
+        print('runtime: {}'.format(self.datetime.now() - self.tic))
+        
+digits = datasets.load_digits()
+X, y = digits.data, digits.target
+param_grid = {"max_depth": [3, None],
+              "max_features": [1, 3, 10],
+              "min_samples_split": [2, 5, 10],
+              "min_samples_leaf": [1, 3, 10],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"],
+              "n_estimators": [10, 20, 40, 80]}
+gs = grid_search.GridSearchCV(RandomForestClassifier(), param_grid=param_grid)
+
+with timeit():
+    gs.fit(X, y)
+
+print(gs)
