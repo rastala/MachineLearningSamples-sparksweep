dbd1ce56a84aa1a2f02852b216a4bce54f94e312
Roope Astala <rastala@users.noreply.github.com>
2017-10-05 16:28:07 -0400

    Update sweep_spark.py

--- sweep_spark.py
+++ sweep_spark.py
@@ -1,10 +1,15 @@
-﻿from sklearn import datasets
+# Use the Azure Machine Learning data collector to log accuracy
+from azureml.logging import get_azureml_logger
+logger = get_azureml_logger()
+
+from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from pyspark import SparkContext
 
-# from sklearn.grid_search import GridSearchCV
-# Use spark_sklearn’s grid search instead:
+# Use spark_sklearn’s grid search:
 from spark_sklearn import GridSearchCV
+import pandas as pd
 
 class timeit():
     from datetime import datetime
@@ -16,18 +21,29 @@ class timeit():
 sc = SparkContext.getOrCreate()
 
 digits = datasets.load_digits()
-X, y = digits.data, digits.target
+x, y = digits.data, digits.target
+
+# Create hold-out test dataset
+x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.25)
+
 param_grid = {"max_depth": [3, None],
               "max_features": [1, 3, 10],
-              "min_samples_split": [2, 5, 10],
               "min_samples_leaf": [1, 3, 10],
               "bootstrap": [True, False],
               "criterion": ["gini", "entropy"],
               "n_estimators": [10, 20, 40, 80]}
 
-gs = GridSearchCV(sc=sc, estimator=RandomForestClassifier(), param_grid=param_grid)
+gs = GridSearchCV(sc=sc, estimator=RandomForestClassifier(), cv=4,param_grid=param_grid,refit=True)
 
 with timeit():
-    gs.fit(X, y)
+    gs.fit(x_train, y_train)
 
-print(gs)
+results = pd.DataFrame(gs.cv_results_)
+print(results.sort_values(['mean_test_score'],ascending=False)[0:10])
+
+# Validate accuracy of best model against hold-out data
+best_model = gs.best_estimator_
+test_accuracy = best_model.score(x_test,y_test)
+print(test_accuracy)
+
+logger.log('Best model accuracy',test_accuracy)
